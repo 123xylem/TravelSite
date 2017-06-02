08459a491c13b983e064323ee8509aaf156ec8a3
Chris <cucullen111@lycos.com>
2017-06-02 12:33:38 +1000

    finished sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,14 @@
 import $ from "jquery";
 import RevealOnScroll from "./modules/revealOnScroll";
 import MobileMenu from "./modules/MobileMenu";
+import StickyHeader from "./modules/StickyHeader";
 
+
+
+//Below we create instances of our constructors defined in our imported modules.
 var menu = new MobileMenu();
+
  new RevealOnScroll($(".feature-item"), "85%");
-  new RevealOnScroll($(".testimonial"), "60%");
+ new RevealOnScroll($(".testimonial"), "60%");
+
+var stickyHeader = new StickyHeader;
---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,80 @@
+import $  from "jquery";
+import waypoint from "../../../../node_modules/waypoints/lib/noframework.waypoints";
+import smoothScroll from "jquery-smooth-scroll";
+
+class StickyHeader{
+	constructor(){
+  		this.siteHeader = $(".site-header");
+  		this.headerTriggerElement = $(".large-hero__title");
+  		this.siteHeaderLinks =$(".primary-nav a");
+        this.createHeaderWaypoint();
+        this.pageSections =$(".page-section");
+        this.createPageSectionWaypoints();
+        this.addSmoothScrolling();
+	}
+
+
+
+addSmoothScrolling(){                //function that adds smoothScroll npm package to our headerlink navs
+	this.siteHeaderLinks.smoothScroll();
+}
+	createHeaderWaypoint(){
+		var that = this;
+		new Waypoint({
+			element:this.headerTriggerElement[0],   //waypoints expects js dom element not js.. so jquery item 0 is always a dom element
+		
+			handler: function(direction){  //waypoints reads direction
+				if(direction== "down"){           //if down         
+				 that.siteHeader.addClass("site-header--dark"); //add class
+                   				}
+                   				else{
+                   					that.siteHeader.removeClass("site-header--dark");  //when moving up through trigger remove class!
+                   				}
+			}
+		});
+	}
+
+
+	createPageSectionWaypoints(){
+		var that =this;                           //make that = to this of above object.
+ 			this.pageSections.each(function(){
+ 					var currentPageSection = this;  //set a this for each page section
+
+ 			 		new Waypoint({
+ 			 			element:currentPageSection,       //for each pagesection
+ 				 			handler: function(direction){ 	
+ 				 				if(direction=="down"){
+
+ 			 				var matchingHeaderLink= currentPageSection.getAttribute("data-matching-link");
+ 			 				//make a var that == the matching link att in the html
+  							that.siteHeaderLinks.removeClass("is-current-link");
+  							//make sure the links arent already lit up
+     				 			$(matchingHeaderLink).addClass("is-current-link");
+     				 	  //make the corresponding link light up with that class
+                                                      }
+ 				 			}, 
+ 				 				offset:"25%"  //when trigger is 30% from top of screen trigger event
+
+ 				 				});
+
+ 			 			new Waypoint({
+ 			 			element:currentPageSection,       //for each pagesection
+ 				 			handler: function(direction){ 	
+ 				 				if(direction=="up"){
+
+ 			 				var matchingHeaderLink= currentPageSection.getAttribute("data-matching-link");
+ 			 				//make a var that == the matching link att in the html
+  							that.siteHeaderLinks.removeClass("is-current-link");
+  							//make sure the links arent already lit up
+     				 			$(matchingHeaderLink).addClass("is-current-link");
+     				 	  //make the corresponding link light up with that class
+                                                      }
+ 				 			}, 
+ 				 				offset:"-40%"  //when trigger is 40% from top of screen trigger event
+ 	 				 		});
+		})
+	}
+
+}
+
+export default StickyHeader;
